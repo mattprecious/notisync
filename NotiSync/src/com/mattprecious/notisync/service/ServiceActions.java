1e92822924df73569d5cbff81cd04ff22b77395e
Matthew Precious <matt@mattprecious.com>
2013-03-04 01:17:14 -0500

    Missing file from 3f6ca1d

---
+++ NotiSync/src/com/mattprecious/notisync/service/ServiceActions.java
@@ -0,0 +1,8 @@
+
+package com.mattprecious.notisync.service;
+
+public class ServiceActions {
+    public final static String ACTION_UPDATE_TIMER = "com.mattprecious.notisync.intent.action.UPDATE_TIMER";
+    public final static String ACTION_SERVICE_STARTED = "com.mattprecious.notisync.intent.action.SERVICE_STARTED";
+    public final static String ACTION_SERVICE_STOPPED = "com.mattprecious.notisync.intent.action.SERVICE_STOPPED";
+}
