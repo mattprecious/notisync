d0718721714c6ac0abd7f43d90962a08cb6857d6
Matthew Precious <matt@mattprecious.com>
2013-03-16 00:39:13 -0400

    Restart the settings activity on mode change in multipane

--- NotiSync/src/com/mattprecious/notisync/preferences/GeneralPreferenceFragment.java
+++ NotiSync/src/com/mattprecious/notisync/preferences/GeneralPreferenceFragment.java
@@ -2,42 +2,51 @@
 package com.mattprecious.notisync.preferences;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
 import com.mattprecious.notisync.R;
-import com.mattprecious.notisync.util.Preferences;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class GeneralPreferenceFragment extends PreferenceFragment {
+public class GeneralPreferenceFragment extends PreferenceFragment implements
+        OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.global_general_preferences);
 
-        findPreference(Preferences.KEY_GLOBAL_MODE).setOnPreferenceChangeListener(
-                new OnPreferenceChangeListener() {
+        getPreferenceManager().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
 
-                    @Override
-                    public boolean onPreferenceChange(Preference preference, Object newValue) {
-                        if (((PreferenceActivity) getActivity()).onIsMultiPane()) {
-//                            Intent intent = new Intent(getActivity(), SettingsActivity.class);
-//                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-//                            intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT,
-//                                    GeneralPreferenceFragment.class.getName());
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(
+                this);
+    }
 
-                            getActivity().finish();
-//                            startActivity(intent);
-//                            getActivity().overridePendingTransition(0, 0);
-                        }
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        // yes, there is already a listener in SettingsActivity, but may as well
+        // put this here since it's only applicable in a fragment environment
+        // plus, don't need to hack up the static listener class to take a
+        // reference to the activity just to do this
+        if (((PreferenceActivity) getActivity()).onIsMultiPane()) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT,
+                    GeneralPreferenceFragment.class.getName());
 
-                        return true;
-                    }
-                });
+            getActivity().finish();
+            startActivity(intent);
+            getActivity().overridePendingTransition(0, 0);
+        }
     }
 }
